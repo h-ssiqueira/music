16336be241d541cb005141e7368f634425809913
Henrique <hss@outlook.com.br>
2023-12-02 18:35:10 -0300

    metallica ride the lighning songs

--- src/main/java/com/hss/music/config/DatabaseConfig.java
+++ src/main/java/com/hss/music/config/DatabaseConfig.java
@@ -1,6 +1,9 @@
 package com.hss.music.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import liquibase.servicelocator.LiquibaseService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +15,17 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
 
     @Bean(name = "musicDatasource")
-    @ConfigurationProperties(prefix="spring.datasource")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource getMusicConnection() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        var dataSourceBuilder = DataSourceBuilder.create();
         return dataSourceBuilder.type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.liquibase")
+    public SpringLiquibase getSpringLiquibaseProperties(@Qualifier("musicDatasource") DataSource dataSource){
+        var config = new SpringLiquibase();
+        config.setDataSource(dataSource);
+        return config;
     }
 }
