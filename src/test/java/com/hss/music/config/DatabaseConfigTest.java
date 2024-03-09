16bc05e0d9cd7bab838b753744040f1aa3094c6c
Henrique <hss@outlook.com.br>
2024-03-09 10:52:01 -0300

    fixing tests and images

--- src/test/java/com/hss/music/config/DatabaseConfigTest.java
+++ src/test/java/com/hss/music/config/DatabaseConfigTest.java
@@ -2,13 +2,17 @@ package com.hss.music.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:application-test.yml")
+@EnableAutoConfiguration(exclude = {LiquibaseAutoConfiguration.class})
 class DatabaseConfigTest {
 
     @Autowired
